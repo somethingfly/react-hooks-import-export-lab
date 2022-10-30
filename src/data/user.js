d1e667aea2dcb3a200dec9cb2ebed47c85030a4b
Alfred <somethingfly@somethingfly.com>
2022-10-30 16:47:46 -0700

    commit tree 1d81b56bd661afd96933e975554086cf0a64a4b0

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,16 @@
+// THIS USER DID NOT MAKE THIS
+// Unless Liza is "Philcrifo" per https://imgur.com/gallery/mV8PQxj
+// And "PhilCrifo" didnt make it completely, unless "PhilCrifo" is Nedroid / Anthony Clark
+// https://nedroidcomics.tumblr.com/post/41879001445/the-internet
+// Also, notice the second panel shows a distinct look of distain, which is never captured in the gif
+// Meaning not only did PhilCrifo (probably) steal this, he lost a layer of meaning in the process
+// Buy CRAMMERZ!
+
+
 const username = "Liza";
 const city = "New York";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
+export {username, city, image};
